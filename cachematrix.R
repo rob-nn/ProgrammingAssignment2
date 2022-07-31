aa1544aa4e9926b6e31691bb1309696b89c11cd3
rob-nn <robertoaguiarlima@protonmail.com>
2022-07-31 10:13:11 -0300

    Excise done

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  data <- x
+  i <- NULL
+  get <- function () data
+  set <- function(x) {
+    data <<- x
+    i <<- NULL
+  }
+  getinverse <- function(...) i
+  setinverse <- function(inv) i <<- inv
+  list (set=set, get=get, getinverse=getinverse, setinverse=setinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if (!is.null(m)) {
+    return (m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m
 }
