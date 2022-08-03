9049a3bc596de86aa0e02446d9afa9004108f8a9
rob-nn <robertoaguiarlima@protonmail.com>
2022-08-03 09:10:18 -0300

    Revised version with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,49 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Functio to make a matrix cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  # The original matrix
   data <- x
+  # the inverse result
   i <- NULL
-  get <- function () data
+  # Returns the original matrix
+  get <- function () {
+    data
+  }
+  # If a new matrix is seted, reset the inverse
   set <- function(x) {
     data <<- x
     i <<- NULL
   }
-  getinverse <- function(...) i
-  setinverse <- function(inv) i <<- inv
+  # returns the inverse value
+  getinverse <- function(...) {
+    i
+  }
+  # set the inverse to a new value
+  setinverse <- function(inv) {
+    i <<- inv
+  }
+  
+  # reference list to all functions
   list (set=set, get=get, getinverse=getinverse, setinverse=setinverse)
 }
 
 
-## Write a short comment describing this function
+## Solve the inverse to a matrix
 
 cacheSolve <- function(x, ...) {
+  # Verify if the inverse for the matrix is already loaded
+  # If not, solve and load the inverse
   m <- x$getinverse()
   if (!is.null(m)) {
+    print("Returning the loaded inverse")
     return (m)
   }
   data <- x$get()
   m <- solve(data,...)
   x$setinverse(m)
+  print("Returning the new solved inverse")
   m
 }
